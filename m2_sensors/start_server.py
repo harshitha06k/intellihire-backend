db5c7fa3d0cb1f7c8f21bcebe861a7f170d4eabb
harshitha06k <harshitha06k@gmail.com>
2026-05-13 11:39:15 +0530

    Update start_server.py

--- m2_sensors/start_server.py
+++ m2_sensors/start_server.py
@@ -1,3 +1,10 @@
+import sys
+import os
+from pathlib import Path
+import uvicorn
+
+sys.path.insert(0, str(Path(__file__).parent))
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8001))
     host = "0.0.0.0"
